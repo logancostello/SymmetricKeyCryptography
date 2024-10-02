d1ee54947d97d6a3ebff64c18f06981592aba142
Logan Costello <logancostello2022@gmail.com>
2024-10-02 14:35:53 -0700

    basic func structure

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+
+# Takes a file, and returns the encrypted version using ECB block cipher
+def encrypt_ecb(file):
+    pass
+
+# Takes a file, and returns the encrypted version using CBC block cipher
+def encrypt_cbc(file):
+    pass
+
+
 if __name__ == '__main__':
     print("Hello world")
